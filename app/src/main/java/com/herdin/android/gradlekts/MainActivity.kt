4cbe7af753094bb65329e9cbf2f4f27225106559
dpq <1692303173@qq.com>
2021-09-10 09:34:36 +0800

    app模块Activity继承基类Activity

--- app/src/main/java/com/herdin/android/gradlekts/MainActivity.kt
+++ app/src/main/java/com/herdin/android/gradlekts/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.herdin.android.gradlekts
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.herdin.android.lib.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
