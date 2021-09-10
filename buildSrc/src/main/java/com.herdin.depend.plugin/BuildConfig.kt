01a8166b4fd0349e747840b891391d0a10584749
dpq <1692303173@qq.com>
2021-09-10 09:35:12 +0800

    构建dependency模块

---
+++ buildSrc/src/main/java/com.herdin.depend.plugin/BuildConfig.kt
@@ -0,0 +1,20 @@
+package com.herdin.depend.plugin
+
+/**
+ * Copyright © 2014-2021, ArcVideo 杭州当虹科技股份有限公司
+ *
+ * @author: herdin
+ * @email： heding@arcvideo.com
+ * @date: 2021/9/10
+ * @desc: 配置和 Build 相关的
+ * @version: V-1.0.0
+ **/
+
+object BuildConfig {
+    const val compileSdk = 30
+    const val buildTools = "29.0.3"
+    const val minSdk = 21
+    const val targetSdk = 30
+    const val versionCode = 10000
+    const val versionName = "1.0.0"
+}
---
+++ buildSrc/src/main/java/com.herdin.depend.plugin/Dependencies.kt
@@ -0,0 +1,34 @@
+package com.herdin.depend.plugin
+
+/**
+ * Copyright © 2014-2021, ArcVideo 杭州当虹科技股份有限公司
+ *
+ * @author: herdin
+ * @email： heding@arcvideo.com
+ * @date: 2021/9/10
+ * @desc:
+ * @version: V-1.0.0
+ **/
+
+object Version {
+
+}
+object Kotlin{
+
+}
+object AndroidX {
+    const val ktx = "androidx.core:core-ktx:1.3.2"
+    const val appcompat = "androidx.appcompat:appcompat:1.3.1"
+    const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.4"
+}
+object JetPack {
+
+}
+object Google {
+    const val material = "com.google.android.material:material:1.4.0"
+}
+object AndroidTest{
+    const val junit = "junit:junit:4.+"
+    const val ext_junit = "androidx.test.ext:junit:1.1.2"
+    const val espresso_core = "androidx.test.espresso:espresso-core:3.3.0"
+}
