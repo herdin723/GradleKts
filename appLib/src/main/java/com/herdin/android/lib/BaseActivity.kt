8e3f7d3727ccfbf98832d9b49f680624fca1f724
dpq <1692303173@qq.com>
2021-09-10 09:33:23 +0800

    lib模块新增Base基类

---
+++ appLib/src/main/java/com/herdin/android/lib/BaseActivity.kt
@@ -0,0 +1,15 @@
+package com.herdin.android.lib
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Copyright © 2014-2021, ArcVideo 杭州当虹科技股份有限公司
+ *
+ * @author: herdin
+ * @email： heding@arcvideo.com
+ * @date: 2021/9/10
+ * @desc:
+ * @version: V-1.0.0
+ **/
+open class BaseActivity :AppCompatActivity(){
+}
---
+++ appLib/src/main/java/com/herdin/android/lib/BaseApp.kt
@@ -0,0 +1,15 @@
+package com.herdin.android.lib
+
+import android.app.Application
+
+/**
+ * Copyright © 2014-2021, ArcVideo 杭州当虹科技股份有限公司
+ *
+ * @author: herdin
+ * @email： heding@arcvideo.com
+ * @date: 2021/9/10
+ * @desc:
+ * @version: V-1.0.0
+ **/
+open class BaseApp :Application(){
+}
